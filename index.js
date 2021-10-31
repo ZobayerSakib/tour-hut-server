1e0264d09009682af3191b4a8aadf72ea8962e14
ZobayerSakib <zobayersakib44@gmail.com>
2021-10-31 22:45:48 +0600

    data sent

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+
+
 //Middleware settings here
 app.use(cors());
 app.use(express.json())
@@ -26,6 +29,13 @@ async function run() {
             const result = await cursor.toArray(cursor);
             console.log(result)
             res.send(result);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesConnection.findOne(query)
+            console.log(service)
+            res.send(service);
         })
 
         app.post('/services', async (req, res) => {
