5cbb488439fc0aa71cbe032f39f1de9361060352
ZobayerSakib <zobayersakib44@gmail.com>
2021-11-01 15:04:56 +0600

    pandom sate

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
-const app = express();
-const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
 
+//Mongodb server site settings
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ndc9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const port = process.env.PORT || 5000;
+const app = express();
 
 //Middleware settings here
 app.use(cors());
 app.use(express.json())
 
-//Mongodb server site settings
-const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ndc9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 async function run() {
     try {
